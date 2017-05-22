f5de2daec1643426b9a9c69b1a42333489897a75
esi143mhzn <pramish143mhzn@gmail.com>
2017-05-22 09:14:45 +0545

    About us update

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.default')
 @section('content')
-    i am the about page
-    
-
-@stop
+<b><h1 style="background-color:black;color:grey">About Us</h1></b>
+<p>Kathmandu BernHardt College(KBC) is a unique institution designed to disseminate quality education to students. Enriched with nearly three decade of experiences in the field of education beginning right from school level to Bachelor level with wonderful and meaningful contribution to the society have proven it as a centre of educational excellence as well as one of the foremost education institutions in Kathmandu. Considering education as a life time investment, BernHardt is dedicated to provide management, humanities, and science & technology education with its pedagogy of mixing university syllabus with modern technology to budding professionals. From its inception, together with furnished and comfortable classrooms, fully equipped library with books and journals and ample learning opportunities and suitable academic programs in its own corporate building, college enjoys full support and patronage of the parents, well-wishers and students.</p>
+<p><h2 style="background-color:#808080;color:white">Vision</h3>
+The vision of BernHardt is to develop the center of excellence for Management, Technical and Professional education and Research at par with national and international standards, advancing the nation’s interests to fulfill gaps of skilled professionals as well as to compete successfully in the global arena.</p>
+<p><h2 style="background-color:#808080;color:white">Mission</h3>
+The mission of BernHardt is to establish it as world class learning centre that equip students with the skills, ability and knowledge to successfully compete in their chosen discipline. </p><br>
+BernHardt aims to provide a foundation of lifetime learning, dedicate itself to foster intellectual growth, identify needs and constraint to overcome shortcoming which will enable students to compete in the challenging opportunities.
+@endsection
+@stops
